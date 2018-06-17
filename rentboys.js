f0029a4e2b14e1b22f30e2c7999c40af0e462177
Josh Stuible <shrewdlove@gmail.com>
2018-06-17 16:42:14 -0700

    Initial Code Commit

---
+++ rentboys.js
@@ -0,0 +1,8 @@
+$(function(){
+    for(i=0;i<4;i++){
+      $('.buzz_wrapper .text .thing').eq(0).clone().prependTo('.buzz_wrapper .text');
+    }
+    for(i=0;i<10;i++){
+      $('.buzz_wrapper .scanline').eq(0).clone().appendTo('.buzz_wrapper');
+    }
+  });
